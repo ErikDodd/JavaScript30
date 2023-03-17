ed38038a77aa90f548cc173d108a18a8ae242be0
Erik M. Dodd <emdodd27@gmail.com>
2023-03-17 13:37:47 -0700

    Play button is working.Toggle play icon is too

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,44 @@
+// Get Elements
+
+const player = document.querySelector('.player');
+
+const video = player.querySelector('.viewer');
+
+const progress = player.querySelector('.progress');
+
+const progressBar = player.querySelector('.progress__filled');
+
+const toggle = player.querySelector('.toggle');
+
+const skipButtons = player.querySelectorAll('[data-skip]');
+
+const ranges = player.querySelectorAll('.player__slider');
+
+
+// Build Functions
+
+function togglePlay() {
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+   
+}
+
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    console.log(icon);
+    toggle.textContent = icon;
+}
+
+function skip() {
+    console.log('Skipping!');
+
+}
+
+// Hook Up Event Listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
