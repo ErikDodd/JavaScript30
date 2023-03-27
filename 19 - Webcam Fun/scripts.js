d2be172d3972d485a366db2c805b42b1eab0c4ba
Erik M. Dodd <emdodd27@gmail.com>
2023-03-27 13:06:54 -0700

    Finished lesson. Webcam fully working.

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -26,7 +26,11 @@ function paintToCanavas() {
         // Take the pixels out
         let pixels = ctx.getImageData(0, 0, width, height);
         // adjust the pixels 
-        pixels = redEffect(pixels);
+        // pixels = redEffect(pixels);
+
+        // pixels = rgbSplit(pixels);
+        // ctx.globalAlpha = 0.8;
+        pixels = greenScreen(pixels);
         // Put the pixels back 
         ctx.putImageData(pixels, 0, 0);
     }, 16);
@@ -59,6 +63,45 @@ function redEffect(pixels) {
     }
     return pixels;
 }
+
+function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        // red
+        pixels.data[i - 150] = pixels.data[i + 0] + 100;
+        // green
+        pixels.data[i + 500] = pixels.data[i + 1] - 50;
+        // blue
+        pixels.data[i - 550] = pixels.data[i + 2] * 0.5;
+    }
+    return pixels;
+}
+
+function greenScreen(pixels) {
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+        levels[input.name] = input.value;
+    });
+
+    for (i = 0; i < pixels.data.length; i += 4) {
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+
+        if (red >= levels.rmin
+            && green >= levels.gmin
+            && blue >= levels.bmin
+            && red <= levels.rmax
+            && green <= levels.gmax
+            && blue <= levels.bmax) {
+            // Take it out!
+            pixels.data[i + 3] = 0;
+        }
+    }
+    return pixels;
+}
+
 getVideo();
 
 video.addEventListener('canplay', paintToCanavas);
