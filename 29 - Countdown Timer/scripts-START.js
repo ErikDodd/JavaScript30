353ce79889a8f062ccd87857fec166ca6ec62f37
Erik M. Dodd <emdodd27@gmail.com>
2023-04-11 12:22:20 -0700

    Almost done. Still need to hook up the timer to connect when buttons are clicked on the page

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,10 +1,13 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
     displayTimeLeft(seconds); 
+    displayEndTime(then);
 
     countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -24,3 +27,18 @@ function displayTimeLeft(seconds) {
     document.title = display;
     timerDisplay.textContent = display;
 }
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const adjustedHour = hour > 12 ? hour - 12 : hour;
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''
+}${minutes}`;
+}
+
+function startTimer() {
+    const seconds = this.dataset.time;
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
